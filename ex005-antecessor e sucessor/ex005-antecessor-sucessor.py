0803542914cb838ccdf653a998ef8e540a72018f
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2022-03-09 20:36:40 -0300

    Add files via upload
    Predecessor and successor

---
+++ ex005-antecessor e sucessor/ex005-antecessor-sucessor.py
@@ -0,0 +1,9 @@
+#Mostrar o antecessor e o sucessor de um número
+n=int(input('Digite um número: '))
+ant=int(n-1)
+suc=int(n+1)
+
+print('O número digitado é {},\n o antecessor é {},\n e o sucessor é {}'.format(n, ant, suc))
+
+
+print('O número digitado é ', n, 'O antecessor é ', n-1, 'e o sucessor é ', n+1)
