cba7cdb50f5ba33776b10ed85e0ca799f3545ce8
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2022-03-10 17:02:30 -0300

    Add files via upload
    unit converter

---
+++ ex008-Conversor-de-unidades/ex008-ConversãoUnidades.py
@@ -0,0 +1,6 @@
+m = float(input('Digite o valor em metros (m): '))
+
+cm = m*100
+mm = m*1000
+
+print('O valor em metros é {}, \n o valor em centímetros é {}, \n e o valor em milímetros é {}.'.format(m, cm, mm))
