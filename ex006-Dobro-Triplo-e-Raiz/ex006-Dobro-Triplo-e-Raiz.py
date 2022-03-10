b8ed3d29b6c35dbeb79074f44a991111ab29c97c
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2022-03-10 16:59:43 -0300

    Add files via upload
    Double, triple and square root

---
+++ ex006-Dobro-Triplo-e-Raiz/ex006-Dobro-Triplo-e-Raiz.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um número: '))
+nd = n*2
+nt = n*3
+nr = n**0.5
+
+print('O número digitado é {}, \n seu dobro é {}, \n seu triplo é {}, \n e sua raiz quadrada é {:.3f}.'.format(n, nd, nt,nr))
