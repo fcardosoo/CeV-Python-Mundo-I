9da15d2fbbdda5c4a3a6d67bf4e313a2b4b53fd7
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2022-03-10 17:00:06 -0300

    Add files via upload
    Average

---
+++ ex007-Média/ex007-Média.py
@@ -0,0 +1,7 @@
+n1 = float(input('digite a primeira nota: '))
+n2 = float(input('digite a segunda nota: '))
+
+m = (n1+n2)/2
+
+print('A nota 1 é {}, \na nota 2 é {}, \n'
+      'e a média é {} '.format(n1,n2,m))
