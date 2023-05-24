df32f901b8ee221e0cd37734115823d0141685d2
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-05-24 07:09:50 -0300

    Create Aula 08.py

---
+++ Aulas/Aula 08.py
@@ -0,0 +1,13 @@
+#Importando bibliotecas para o python
+
+#importanto toda a biblioteca... use:
+
+#import.math (por exemplo)
+
+#Para importar apenas uma funcionalidade da biblioteca math use:
+
+#from.math.import.sqrt (por exemplo)
+
+#para importar 2 funcionalidades da mesma biblioteca use ','
+
+#ex.: from math import sqrt,ceil,floor, trunc,pow,factorial, etc...
